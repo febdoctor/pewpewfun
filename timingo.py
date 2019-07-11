7e4b0337349074dd3c6cb16e323d5d8f4ce4db0a
Francois Beerten <fbeerten.git@b10eng.com>
2019-07-11 17:35:48 +0200

    Talk timer "timingo" application

---
+++ timingo.py
@@ -0,0 +1,64 @@
+import pew
+
+pew.init()
+screen = pew.Pix()
+background = pew.Pix.from_iter((
+    (1, 0, 1, 0, 1, 0, 1, 0),
+    (0, 1, 0, 1, 0, 1, 0, 1),
+    (1, 0, 1, 0, 1, 0, 1, 0),
+    (0, 1, 0, 1, 0, 1, 0, 1),
+    (1, 0, 1, 0, 1, 0, 1, 0),
+    (0, 1, 0, 1, 0, 1, 0, 1),
+    (1, 0, 1, 0, 1, 0, 1, 0),
+    (0, 1, 0, 1, 0, 1, 0, 1),
+))
+
+sesstime = 45
+screen.blit(background)
+while pew.keys():
+    pass
+while True:
+    keys = pew.keys()
+    if keys & pew.K_UP:
+        sesstime+=5
+    if keys & pew.K_DOWN and sesstime > 5:
+        sesstime-=5
+    if keys & pew.K_O:
+        break
+    txt = pew.Pix.from_text('%02d' % sesstime)
+    screen.blit(txt, 0, 0)
+    pew.show(screen)
+    pew.tick(1/5)
+
+while pew.keys():
+    pass
+print('NEXT')
+secs = 0
+rem = sesstime
+screen.box(0)
+clk = 8
+steps = 8
+ticks = 0
+fg = 3
+limits=[10, 5, 2, 1]
+while rem > 0:
+    keys = pew.keys()
+    m = 0
+    for i in range(8):
+        clr = min(max(0, secs - m), 7) // 2
+        screen.pixel(i, 7, color = clr)
+        m += steps
+    ticks+=1
+    if ticks == 4:
+        secs += 1
+        ticks = 0
+    if secs >= 60:
+        secs = 0
+        rem -= 1
+    for i, lim in enumerate(limits):
+        if rem <= lim:
+           fg = 3 - ticks % (i+1)
+    txt = pew.Pix.from_text('%02d' % rem, color=fg)
+    screen.blit(txt, 0, 0)
+    pew.show(screen)
+    pew.tick(1 / 4)
